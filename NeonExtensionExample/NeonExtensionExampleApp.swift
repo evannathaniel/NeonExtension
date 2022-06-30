eae81124d318ccaee24893968fce30bde202106c
evannathaniel <evan.nathaniel0308@gmail.com>
2022-06-30 08:34:53 +0700

    first commit

---
+++ NeonExtensionExample/NeonExtensionExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeonExtensionExampleApp.swift
+//  NeonExtensionExample
+//
+//  Created by Evan N on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct NeonExtensionExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
